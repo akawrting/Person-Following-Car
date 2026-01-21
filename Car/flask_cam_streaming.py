9bdbd3e00cfa3cc898f30a6254cf5cd5dcc3c45e
akawrting <simpilgione@gmail.com>
2026-01-21 15:33:51 +0900

    refactor: motor.py modularization

--- Car/flask_cam_streaming.py
+++ Car/flask_cam_streaming.py
@@ -2,59 +2,14 @@ from flask import Flask, Response, render_template
 from picamera2 import Picamera2
 import socket, time, threading, cv2
 from gpiozero import PWMOutputDevice, DigitalOutputDevice, DistanceSensor, RGBLED
+import motor
 
-# GPIO pin setting
-leftMotorSpeed = PWMOutputDevice(18)
-leftMotorReverse = DigitalOutputDevice(22) 
-leftMotorForward = DigitalOutputDevice(27) 
-rightMotorSpeed = PWMOutputDevice(23)
-rightMotorReverse = DigitalOutputDevice(25)
-rightMotorForward = DigitalOutputDevice(24)
 
 front_sensor = DistanceSensor(echo=10, trigger=9, max_distance=2.0)
 left_sensor = DistanceSensor(echo=17, trigger=4, max_distance=2.0)
 right_sensor = DistanceSensor(echo=8, trigger=7, max_distance=2.0)
 
-# motor operation
-def stop_motors():
-    leftMotorSpeed.value = 0.0
-    rightMotorSpeed.value = 0.0
 
-def move_forward():
-    leftMotorForward.value, leftMotorReverse.value = 1, 0
-    rightMotorForward.value, rightMotorReverse.value = 1, 0
-    leftMotorSpeed.value = 1
-    rightMotorSpeed.value = 1
-
-def move_backward():
-    leftMotorForward.value, leftMotorReverse.value = 0, 1
-    rightMotorForward.value, rightMotorReverse.value = 0, 1
-    leftMotorSpeed.value = 1
-    rightMotorSpeed.value = 1
-
-def turn_left_soft():
-    leftMotorForward.value, leftMotorReverse.value = 0, 0
-    rightMotorForward.value, rightMotorReverse.value = 1, 0
-    leftMotorSpeed.value = 1
-    rightMotorSpeed.value = 1
-
-def turn_right_soft():
-    leftMotorForward.value, leftMotorReverse.value = 1, 0
-    rightMotorForward.value, rightMotorReverse.value = 0, 0
-    leftMotorSpeed.value = 1
-    rightMotorSpeed.value = 1 
-
-def turn_left():
-    leftMotorForward.value, leftMotorReverse.value = 0, 1
-    rightMotorForward.value, rightMotorReverse.value = 1, 0
-    leftMotorSpeed.value = 0.4
-    rightMotorSpeed.value = 0.4
-
-def turn_right():
-    leftMotorForward.value, leftMotorReverse.value = 1, 0
-    rightMotorForward.value, rightMotorReverse.value = 0, 1
-    leftMotorSpeed.value = 0.4
-    rightMotorSpeed.value = 0.4
 
 def auto_following():
     global auto_mode
@@ -155,19 +110,19 @@ def control(web_cmd):
     print("web command:", web_cmd)
 
     if web_cmd == 'forward':
-        move_forward()
+        motor.move_forward()
         print("forward")
     elif web_cmd == 'backward':
-        move_backward()
+        motor.move_backward()
         print("backward")
     elif web_cmd == 'left':
-        turn_left_soft()
+        motor.turn_left_soft()
         print("left")
     elif web_cmd == 'right':
         turn_right_soft()
         print("right")
     elif web_cmd == 'stop':
-        stop_motors()
+        motor.stop_motors()
         print("stop")
     elif web_cmd == 'power':
         if not auto_mode:
@@ -176,7 +131,7 @@ def control(web_cmd):
         else:
             auto_mode = False
             time.sleep(0.2)
-            stop_motors()
+            motor.stop_motors()
             print("auto mode off")
 
     return "OK"
